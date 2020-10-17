58a4b7d0fd7d4de007cda64903e491ad0a8e146f
Kayla Altepeter <kayla@kaylaaltepeter.com>
2020-10-16 19:18:10 -0500

    feat(builder): creating
    - adding ts-node for running jasmine in ts
    - add builder and schema

--- src/svg-icons-builder/index.ts
+++ src/svg-icons-builder/index.ts
@@ -1,10 +1,30 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+  
+import { BuilderOutput, createBuilder } from '@angular-devkit/architect';
+import * as childProcess from 'child_process';
+import { JsonObject } from '@angular-devkit/core';
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function svgIconsBuilder(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+interface Options extends JsonObject {
+  command: string;
+  args: string[];
 }
+
+export default createBuilder<Options>((options, context) => {
+console.log("options, context", options)
+  return new Promise<BuilderOutput>((resolve, reject) => {
+    context.reportStatus(`Executing "${options.command}"...`);
+    const child = childProcess.spawn(options.command, options.args, { stdio: 'pipe' });
+
+    child.stdout.on('data', (data) => {
+      context.logger.info(data.toString());
+    });
+    child.stderr.on('data', (data) => {
+      context.logger.error(data.toString());
+      reject();
+    });
+
+    context.reportStatus(`Done.`);
+    child.on('close', code => {
+      resolve({ success: code === 0 });
+    });
+  });
+});
--- src/svg-icons-builder/index_spec.ts
+++ src/svg-icons-builder/index_spec.ts
@@ -1,16 +1,48 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import * as path from 'path';
-
-
-const collectionPath = path.join(__dirname, '../collection.json');
-
+import { Architect } from '@angular-devkit/architect';
+import { TestingArchitectHost } from '@angular-devkit/architect/testing';
+import { logging, schema } from '@angular-devkit/core';
+const { join } = require('path');
 
 describe('svg-icons-builder', () => {
-  it('works', async () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner.runSchematicAsync('svg-icons-builder', {}, Tree.empty()).toPromise();
+  let architect: Architect;
+  let architectHost: TestingArchitectHost;
 
-    expect(tree.files).toEqual([]);
+  beforeEach(async () => {
+    const registry = new schema.CoreSchemaRegistry();
+    registry.addPostTransform(schema.transforms.addUndefinedDefaults);
+    const workspaceRoot = join(__dirname, '..', '..');
+
+    // TestingArchitectHost() takes workspace and current directories.
+    // Since we don't use those, both are the same in this case.
+    architectHost = new TestingArchitectHost(__dirname, __dirname);
+    architect = new Architect(architectHost, registry);
+
+    // This will either take a Node package name, or a path to the directory
+    // for the package.json file.
+    await architectHost.addBuilderFromPackage(workspaceRoot);
+   console.log('#', Array.from((architectHost as any)._builderMap.keys()))
+  });
+
+  it('works', async () => {
+    const logger = new logging.Logger('');
+    const logs: string[] = [];
+    logger.subscribe(ev => logs.push(ev.message));
+
+    // A "run" can have multiple outputs, and contains progress information.
+    const run = await architect.scheduleBuilder('@angular-extensions/svg-icons-builder:svg-icons-builder', {
+      command: 'node',
+      args: ['--print', '\'foo\''],
+    }, { logger });  // We pass the logger for checking later.
+
+    // The "result" member (of type BuilderOutput) is the next output.
+    await run.result;
+
+    // Stop the builder from running. This stops Architect from keeping
+    // the builder-associated states in memory, since builders keep waiting
+    // to be scheduled.
+    await run.stop();
+
+    // Expect that foo was logged
+    expect(logs).toContain('foo');
   });
 });
