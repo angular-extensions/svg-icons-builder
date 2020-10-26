3095b15a13bc8981fe214552cbd8d63a89754355
kreuzerk <kevin.kreuzer90@icloud.com>
2020-10-26 18:24:48 +0100

    style: 💄 rerun prettier

--- src/ng-add/index.spec.ts
+++ src/ng-add/index.spec.ts
@@ -1,15 +1,13 @@
-import { Tree } from "@angular-devkit/schematics";
-import { SchematicTestRunner } from "@angular-devkit/schematics/testing";
-import * as path from "path";
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
 
-const collectionPath = path.join(__dirname, "../collection.json");
+const collectionPath = path.join(__dirname, '../collection.json');
 
-describe("ng-add", () => {
-  it("works", async () => {
-    const runner = new SchematicTestRunner("schematics", collectionPath);
-    const tree = await runner
-      .runSchematicAsync("ng-add", {}, Tree.empty())
-      .toPromise();
+describe('ng-add', () => {
+  it('works', async () => {
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    const tree = await runner.runSchematicAsync('ng-add', {}, Tree.empty()).toPromise();
 
     expect(tree.files).toEqual([]);
   });
--- src/svg-icons-builder/index.spec.ts
+++ src/svg-icons-builder/index.spec.ts
@@ -1,16 +1,16 @@
-import { Architect } from "@angular-devkit/architect";
-import { TestingArchitectHost } from "@angular-devkit/architect/testing";
-import { logging, schema } from "@angular-devkit/core";
-import { Delimiter } from "svg-to-ts/src/lib/generators/code-snippet-generators";
-import { CommonConversionOptions } from "svg-to-ts/src/lib/options/conversion-options";
-const { join } = require("path");
+import { Architect } from '@angular-devkit/architect';
+import { TestingArchitectHost } from '@angular-devkit/architect/testing';
+import { logging, schema } from '@angular-devkit/core';
+import { Delimiter } from 'svg-to-ts/src/lib/generators/code-snippet-generators';
+import { CommonConversionOptions } from 'svg-to-ts/src/lib/options/conversion-options';
+const { join } = require('path');
 
-describe("svg-icons-builder", () => {
+describe('svg-icons-builder', () => {
   let architect: Architect;
   let architectHost: TestingArchitectHost;
   const defaultCommonOptions: CommonConversionOptions = {
-    srcFiles: ["./dinosaur-icons/icons/**/*.svg"],
-    outputDirectory: "./dist/test/dinosaur-icons",
+    srcFiles: ['./dinosaur-icons/icons/**/*.svg'],
+    outputDirectory: './dist/test/dinosaur-icons',
     svgoConfig: {
       plugins: [
         {
@@ -24,37 +24,37 @@ describe("svg-icons-builder", () => {
   beforeEach(async () => {
     const registry = new schema.CoreSchemaRegistry();
     registry.addPostTransform(schema.transforms.addUndefinedDefaults);
-    const workspaceRoot = join(__dirname, "..", "..");
+    const workspaceRoot = join(__dirname, '..', '..');
 
     // TestingArchitectHost() takes workspace and current directories.
     // Since we don't use those, both are the same in this case.
-    architectHost = new TestingArchitectHost("/root", "/root");
+    architectHost = new TestingArchitectHost('/root', '/root');
     architect = new Architect(architectHost, registry);
 
     // This will either take a Node package name, or a path to the directory
     // for the package.json file.
     await architectHost.addBuilderFromPackage(workspaceRoot);
-    console.log("#", Array.from((architectHost as any)._builderMap.keys()));
+    console.log('#', Array.from((architectHost as any)._builderMap.keys()));
   });
 
-  it("generates constants for icons", async () => {
-    const logger = new logging.Logger("");
+  it('generates constants for icons', async () => {
+    const logger = new logging.Logger('');
     const logs: string[] = [];
     logger.subscribe((ev) => logs.push(ev.message));
 
     // A "run" can have multiple outputs, and contains progress information.
     const run = await architect.scheduleBuilder(
-      "@angular-extensions/svg-icons-builder:svg-icons-builder",
+      '@angular-extensions/svg-icons-builder:svg-icons-builder',
       {
         ...defaultCommonOptions,
-        conversionType: "constants",
-        fileName: "dinosaur-icons",
-        typeName: "dinosaurIcon",
+        conversionType: 'constants',
+        fileName: 'dinosaur-icons',
+        typeName: 'dinosaurIcon',
         generateType: true,
         generateTypeObject: true,
         generateCompleteIconSet: true,
-        prefix: "dinosaurIcon",
-        interfaceName: "DinosaurIcon",
+        prefix: 'dinosaurIcon',
+        interfaceName: 'DinosaurIcon',
       },
       { logger }
     ); // We pass the logger for checking later.
@@ -69,6 +69,6 @@ describe("svg-icons-builder", () => {
     await run.stop();
 
     expect(logs).toContain('We are using the conversion type "constants"');
-    expect(logs).toContain("svg-to-ts completed.");
+    expect(logs).toContain('svg-to-ts completed.');
   });
 });
